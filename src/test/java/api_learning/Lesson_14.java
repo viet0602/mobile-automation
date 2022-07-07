94de67e78321fdeac79b06081d4a6cd25e82c743
viet0602 <viet0602@gmail.com>
2022-07-07 11:44:33 +0700

    udpate code for lab14

--- src/test/java/api_learning/Lesson_14.java
+++ src/test/java/api_learning/Lesson_14.java
@@ -7,20 +7,25 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.URL;
 
 public class Lesson_14 {
+
     public static void main(String[] args) {
+        //Send request into appium server > as
         AppiumDriver<MobileElement> appiumDriver = null;
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName","Android");
         capabilities.setCapability("automationName","uiautomator2");
-        capabilities.setCapability("udid","id of device");
+        capabilities.setCapability("udid","R58N7200YDJ");
         capabilities.setCapability("appPackage","com.wdiodemoapp");
         capabilities.setCapability("appActivity","com.wdiodemoapp.MainActivity");
 
         try {
             URL appiumServer = new URL(" http://localhost:4723/wd/hub");
             appiumDriver = new AndroidDriver <MobileElement> (appiumServer,capabilities);
+
+            Thread.sleep(3000);
         } catch (Exception e) {
             e.printStackTrace();
         }
+        if (appiumDriver != null) appiumDriver.quit();
     }
 }
