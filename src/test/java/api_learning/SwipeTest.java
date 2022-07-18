c89e23cf4b2f9203c343021e9325623ae50f2cfb
viet0602 <viet0602@gmail.com>
2022-07-18 23:48:26 +0700

    Add lab 16

---
+++ src/test/java/api_learning/SwipeTest.java
@@ -0,0 +1,64 @@
+package api_learning;
+
+import driver.DriverFactory;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.Dimension;
+import platform.Platform;
+
+import java.time.Duration;
+
+public class SwipeTest {
+
+    public static void main(String[] args) {
+
+        AppiumDriver<MobileElement> appiumDriver = DriverFactory.getDriver(Platform.ANDROID);
+        try {
+            MobileElement navSwipeBtnElem = appiumDriver.findElement(MobileBy.AccessibilityId("Swipe"));
+            navSwipeBtnElem.click();
+
+            //Get mobile window size
+            Dimension windowSize = appiumDriver.manage().window().getSize();
+            int screenHeight = windowSize.getHeight();
+            int screenWidth = windowSize.getWidth();
+
+            // Calculate touch points
+            int xStartPoint = 65 * screenWidth / 100;
+            int xEndPoint = 0;
+
+            int yStartPoint = 70 * screenHeight / 100;
+            int yEndPoint = 70 * screenHeight / 100;
+
+            // Convert coordinates > Point Option
+            PointOption startPoint = new PointOption<>().withCoordinates(xStartPoint, yStartPoint);
+            PointOption endPoint = new PointOption<>().withCoordinates(xEndPoint, yEndPoint);
+
+            // Using TouchAction to swipe
+            TouchAction touchAction = new TouchAction<>(appiumDriver);
+            for (int time = 0; time < 6; time++) {
+                touchAction
+                        .press(startPoint)
+                        .waitAction(new WaitOptions().withDuration(Duration.ofMillis(500)))
+                        .moveTo(endPoint)
+                        .release()
+                        .perform();
+            }
+            for (int time = 0; time < 6; time++) {
+                touchAction
+                        .press(endPoint)
+                        .waitAction(new WaitOptions().withDuration(Duration.ofMillis(500)))
+                        .moveTo(startPoint)
+                        .release()
+                        .perform();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        appiumDriver.quit();
+    }
+}
+
