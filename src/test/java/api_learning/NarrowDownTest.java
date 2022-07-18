8f5da6d099fedaa463d697f99716d46b2287f817
viet0602 <viet0602@gmail.com>
2022-07-18 23:48:26 +0700

    Add lab 16

---
+++ src/test/java/api_learning/NarrowDownTest.java
@@ -0,0 +1,77 @@
+package api_learning;
+
+import driver.DriverFactory;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.Dimension;
+import platform.Platform;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class NarrowDownTest {
+
+    public static void main(String[] args) {
+        AppiumDriver<MobileElement> appiumDriver = DriverFactory.getDriver(Platform.ANDROID);
+        try {
+            //Get mobile window size
+            Dimension windowSize = appiumDriver.manage().window().getSize();
+            int screenHeight = windowSize.getHeight();
+            int screenWidth = windowSize.getWidth();
+
+            // Calculate touch points
+            int xStartPoint = 50 * screenWidth / 100;
+            int xEndPoint = 50 * screenWidth / 100;
+
+            int yStartPoint = 0;
+            int yEndPoint = 60 * screenHeight / 100;
+
+            // Convert coordinates > Point Option
+            PointOption startPoint = new PointOption<>().withCoordinates(xStartPoint, yStartPoint);
+            PointOption endPoint = new PointOption<>().withCoordinates(xEndPoint, yEndPoint);
+
+            // Using TouchAction to swipe
+            TouchAction touchAction = new TouchAction<>(appiumDriver);
+            touchAction
+                    .press(startPoint)
+                    .waitAction(new WaitOptions().withDuration(Duration.ofMillis(500)))
+                    .moveTo(endPoint)
+                    .release()
+                    .perform();
+            List<MobileElement> notificationElems =
+                    appiumDriver.findElements(MobileBy.id("android:id/notification_headerless_view_row"));
+            Map<String, String> notificationContents = new HashMap<>();
+            for (MobileElement notificationElem : notificationElems) {
+                MobileElement titleElem = notificationElem.findElement(MobileBy.id("android:id/title"));
+                MobileElement contentElem = notificationElem.findElement(MobileBy.id("android:id/text"));
+
+                notificationContents.put(titleElem.getText().trim(),contentElem.getText().trim());
+            }
+            if(notificationContents.isEmpty()){
+                throw new RuntimeException("Have no notifications");
+            }
+            for(String title:notificationContents.keySet()){
+                  System.out.println("Title: " + title);
+                  System.out.println("Content: " + notificationContents.get(title));
+            }
+
+            //Tear down
+            touchAction
+                    .press(endPoint)
+                    .waitAction(new WaitOptions().withDuration(Duration.ofMillis(500)))
+                    .moveTo(startPoint)
+                    .release()
+                    .perform();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        appiumDriver.quit();
+    }
+}
